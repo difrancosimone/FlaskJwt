08aa36cdacad8dcbcf300495df096c6626229544
simonedifranco <difrancosimone@gmail.com>
2018-11-04 18:26:56 +0100

    added Jwt

--- resources.py
+++ resources.py
@@ -1,15 +1,16 @@
 from flask_restful import Resource, reqparse
 from models import UserModel
+from flask_jwt_extended import (create_access_token, create_refresh_token, jwt_required, jwt_refresh_token_required, get_jwt_identity, get_raw_jwt)
 
 parser = reqparse.RequestParser()
 parser.add_argument('username', help = 'This field cannot be blank', required = True)
 parser.add_argument('password', help = 'This field cannot be blank', required = True)
 
+
 class UserRegistration(Resource):
     def post(self):
         data = parser.parse_args()
 
-        #check if user exist
         if UserModel.find_by_username(data['username']):
             return {'message': 'User {} already exists'.format(data['username'])}
 
@@ -17,23 +18,36 @@ class UserRegistration(Resource):
             username=data['username'],
             password=UserModel.generate_hash(data['password'])
         )
+
         try:
             new_user.save_to_db()
+            access_token = create_access_token(identity=data['username'])
+            refresh_token = create_refresh_token(identity=data['username'])
             return {
-                'message': 'User {} was created'.format( data['username'])
+                'message': 'User {} was created'.format(data['username']),
+                'access_token': access_token,
+                'refresh_token': refresh_token
             }
         except:
             return {'message': 'Something went wrong'}, 500
+
 
 class UserLogin(Resource):
     def post(self):
         data = parser.parse_args()
         current_user = UserModel.find_by_username(data['username'])
+
         if not current_user:
             return {'message': 'User {} doesn\'t exist'.format(data['username'])}
 
         if UserModel.verify_hash(data['password'], current_user.password):
-            return {'message': 'Logged in as {}'.format(current_user.username)}
+            access_token = create_access_token(identity=data['username'])
+            refresh_token = create_refresh_token(identity=data['username'])
+            return {
+                'message': 'Logged in as {}'.format(current_user.username),
+                'access_token': access_token,
+                'refresh_token': refresh_token
+            }
         else:
             return {'message': 'Wrong credentials'}
 
@@ -54,6 +68,7 @@ class TokenRefresh(Resource):
 
 
 class AllUsers(Resource):
+    @jwt_required
     def get(self):
         return UserModel.return_all()
 
