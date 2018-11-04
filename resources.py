5d008b3e765ccf33923f84693e138ac9a926379c
simonedifranco <difrancosimone@gmail.com>
2018-11-04 18:30:11 +0100

    added logout classes

--- resources.py
+++ resources.py
@@ -1,5 +1,5 @@
 from flask_restful import Resource, reqparse
-from models import UserModel
+from models import UserModel, RevokedTokenModel
 from flask_jwt_extended import (create_access_token, create_refresh_token, jwt_required, jwt_refresh_token_required, get_jwt_identity, get_raw_jwt)
 
 parser = reqparse.RequestParser()
@@ -75,9 +75,24 @@ class AllUsers(Resource):
     def delete(self):
         return UserModel.delete_all()
 
+class UserLogoutAccess(Resource):
+    @jwt_required
+    def post(self):
+        jti = get_raw_jwt()['jti']
+        try:
+            revoked_token = RevokedTokenModel(jti=jti)
+            revoked_token.add()
+            return {'message': 'Access token has been revoked'}
+        except:
+            return {'message': 'Something went wrong'}, 500
 
-class SecretResource(Resource):
-    def get(self):
-        return {
-            'answer': 42
-        }
+class UserLogoutRefresh(Resource):
+    @jwt_refresh_token_required
+    def post(self):
+        jti = get_raw_jwt()['jti']
+        try:
+            revoked_token = RevokedTokenModel(jti=jti)
+            revoked_token.add()
+            return {'message': 'Refresh token has been revoked'}
+        except:
+            return {'message': 'Something went wrong'}, 500
