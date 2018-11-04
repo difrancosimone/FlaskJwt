4cb6d8c2eb78df0b6814c79dacfb2e09511d4473
simonedifranco <difrancosimone@gmail.com>
2018-11-04 18:19:01 +0100

    updated project files for login

--- run.py
+++ run.py
@@ -1,8 +1,20 @@
 from flask import Flask
 from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 api = Api(app)
+
+# connect to database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'some-secret-string'
+
+db = SQLAlchemy(app)
+#create database if not exist
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
 import views, models, resources
 
